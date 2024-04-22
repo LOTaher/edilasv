eb45d050a8417a08fa746a7caf124650d49317af
Laith Taher <lotaher04@gmail.com>
2024-04-21 20:29:01 -0700

    chore: project cleanup

--- softbase.go
+++ softbase.go
@@ -2,7 +2,9 @@ package softbase
 
 import (
 	"os"
+    "os/signal"
 	"path/filepath"
+    "syscall"
 
 	"github.com/LOTaher/softbase/cmd"
 	"github.com/LOTaher/softbase/core"
@@ -12,42 +14,58 @@ import (
 var Version = "0.0.1"
 
 type appWrapper struct {
-    core.App
+	core.App
 }
 
 // SoftBase defines a SoftBase application launcher.
 type SoftBase struct {
-    *appWrapper
-    RootCmd *cobra.Command
+	*appWrapper
+	RootCmd *cobra.Command
 }
 
 // Creates a new SoftBase instance with the default configuration.
 func New() *SoftBase {
-    sb := &SoftBase{
-        RootCmd: &cobra.Command{
-            Use:   filepath.Base(os.Args[0]),
-            Short: "SoftBase is a key-value store backend for your next side project.",
-            Version: Version,
-        },
+	sb := &SoftBase{
+		RootCmd: &cobra.Command{
+			Use:     filepath.Base(os.Args[0]),
+			Short:   "SoftBase is a key-value store backend for your next side project.",
+			Version: Version,
+		},
+	}
 
-    }
-    
-    // hide the default help command, only allowing `--help` flag.
-    sb.RootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
+	// hide the default help command, only allowing `--help` flag.
+	sb.RootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
 
-    return sb
+	return sb
 }
 
 func (sb *SoftBase) Start() error {
-    sb.RootCmd.AddCommand(cmd.Serve(sb))
-    
-    return sb.Execute()
+	sb.RootCmd.AddCommand(cmd.Serve(sb))
+
+	return sb.Execute()
 }
 
 func (sb *SoftBase) Execute() error {
-    if err := sb.RootCmd.Execute(); err != nil {
-        os.Exit(1)
-    }
+	done := make(chan bool, 1)
 
+	// listen for interrupt signal to gracefully shutdown the application
+	go func() {
+		sigch := make(chan os.Signal, 1)
+		signal.Notify(sigch, os.Interrupt, syscall.SIGTERM)
+		<-sigch
+
+		done <- true
+	}()
+
+	// execute the root command
+	go func() {
+		sb.RootCmd.Execute()
+
+		done <- true
+	}()
+
+	<-done
+    
+    // TODO, add a graceful shutdown here
     return nil
 }
